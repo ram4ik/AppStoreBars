9131083fa34377184da6e9244477d75e734d4d78
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-24 20:47:45 +0200

    Initial Commit

---
+++ AppStoreBars/AppStoreBarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreBarsApp.swift
+//  AppStoreBars
+//
+//  Created by test on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreBarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
